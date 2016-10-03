577f206e5073fd0a614e9f76a2fc578eec0b0d39
Jason <floresj4@gmail.com>
2016-10-02 22:12:19 -0400

    checking over to spreadful controller

--- src/main/java/com/flores/h2/spreadful/SpreadfulController.java
+++ src/main/java/com/flores/h2/spreadful/SpreadfulController.java
@@ -2,31 +2,26 @@ package com.flores.h2.spreadful;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
-@RestController
+@Controller
 public class SpreadfulController {
 
 	private static final Logger logger = LoggerFactory.getLogger(SpreadfulController.class);
-
-	/**
-	 * put or post
-	 * @param o something to test post
-	 * @return
-	 */
-	@RequestMapping(value = "/test")
-	public HttpEntity<Object> test() {
-		return new ResponseEntity<Object>("testing...", HttpStatus.OK);
-	}
 	
-	@RequestMapping(method = RequestMethod.PUT, value = "/load")
-	public void load() {
-		String filename = "unset-filename.xlsx";
-		logger.debug("loading {}", filename);
+	@GetMapping("/spreadful")
+	public String init(Model model) {
+		model.addAttribute("worksheet", new Worksheet());
+		return "fileupload";
+	}
+
+	@PostMapping("/spreadful/load")
+	public String load(@ModelAttribute Worksheet worksheet) {
+		logger.debug("loading {}", worksheet.getContent());
+		return "result";
 	}
 }
---
+++ src/main/java/com/flores/h2/spreadful/Worksheet.java
@@ -0,0 +1,21 @@
+package com.flores.h2.spreadful;
+
+/**
+ * 
+ * @author Jason
+ */
+public class Worksheet {
+
+	private String content;
+	
+	public Worksheet() {}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+}
