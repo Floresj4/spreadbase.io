cb706739faefb1b6fa9a98469591e5c4e9c38611
Jason <floresj4@gmail.com>
2016-10-03 18:54:08 -0400

    reverting back to appassembler and strict REST api; separate form and function

--- src/main/java/com/flores/h2/spreadful/SpreadfulController.java
+++ src/main/java/com/flores/h2/spreadful/SpreadfulController.java
@@ -2,26 +2,21 @@ package com.flores.h2.spreadful;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class SpreadfulController {
 
-	private static final Logger logger = LoggerFactory.getLogger(SpreadfulController.class);
+	@SuppressWarnings("unused")
+	private static final Logger logger = LoggerFactory
+			.getLogger(SpreadfulController.class);
 	
-	@GetMapping("/spreadful")
-	public String init(Model model) {
-		model.addAttribute("worksheet", new Worksheet());
-		return "fileupload";
-	}
-
-	@PostMapping("/spreadful/load")
-	public String load(@ModelAttribute Worksheet worksheet) {
-		logger.debug("loading {}", worksheet.getContent());
-		return "result";
+	@RequestMapping
+	public HttpEntity<String> spreadful() {
+		return new ResponseEntity<String>("testing...", HttpStatus.OK);
 	}
 }
