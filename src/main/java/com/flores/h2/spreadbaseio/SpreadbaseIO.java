b385aa1063e64d0a1f2599e46fd553b24616fa35
Jason <floresj4@gmail.com>
2016-10-28 16:03:26 -0400

    respond with file impl.

--- src/main/java/com/flores/h2/spreadbaseio/SpreadbaseIO.java
+++ src/main/java/com/flores/h2/spreadbaseio/SpreadbaseIO.java
@@ -2,14 +2,19 @@ package com.flores.h2.spreadbaseio;
 
 import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.flores.h2.spreadbase.Spreadbase;
+import com.flores.h2.spreadbase.util.BuilderUtil;
 import com.flores.h2.spreadbaseio.model.impl.Version;
 
 @RestController
@@ -27,7 +33,7 @@ public class SpreadbaseIO {
 			.getLogger(SpreadbaseIO.class);
 
 	@RequestMapping(value = "/upload", method = RequestMethod.POST)
-	public HttpEntity<String> spreadful(@RequestParam("file") MultipartFile inputFile) {
+	public HttpEntity<Object> spreadful(@RequestParam("file") MultipartFile inputFile) {
 		logger.debug("upload received: {}", inputFile.getOriginalFilename());
 		
 		//create file for spreadbase
@@ -45,14 +51,14 @@ public class SpreadbaseIO {
 				logger.error("spreadbase invocation exception: {}", e.getMessage());
 				throw e;
 			}
+			
+			return getResponseFile(BuilderUtil.fileAsH2File(tmp));
 		}
 		catch (Exception e) {
-			return new ResponseEntity<String>("failure...", HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<Object>("failure...", HttpStatus.INTERNAL_SERVER_ERROR);
 		} finally {
 			tmp.deleteOnExit();
 		}
-
-		return new ResponseEntity<String>("success...", HttpStatus.OK);
 	}
 	
 	@GetMapping("/info")
@@ -61,4 +67,24 @@ public class SpreadbaseIO {
 				new Version("spreadbase", "1.0.0"),
 				HttpStatus.OK);
 	}
+	
+	private static ResponseEntity<Object> getResponseFile(File f) throws FileNotFoundException {
+
+		//build the response header
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+		headers.add("Access-Control-Allow-Origin", "*");
+		headers.add("Access-Control-Allow-Methods", "GET, POST, PUT");
+		headers.add("Access-Control-Allow-Headers", "Content-Type");
+		headers.add("Content-Disposition", "filename=" + f.getName());
+		headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+		headers.add("Pragma", "no-cache");
+		headers.add("Expires", "0");
+
+		headers.setContentLength(f.getTotalSpace());  
+		return new ResponseEntity<Object>(
+				new InputStreamResource(new FileInputStream(f)),
+				headers, HttpStatus.OK);
+	}
+	
 }
